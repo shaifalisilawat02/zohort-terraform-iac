14edb625ca99381a4a181800c5b32abafc731a74
shaifalisilawat02 <shaifalisilawat02@gmail.com>
2025-09-27 23:32:29 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+  default     = "zohort-devops-shaifali"
+}
